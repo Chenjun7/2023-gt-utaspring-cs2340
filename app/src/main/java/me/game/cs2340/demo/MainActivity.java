dd03782f0665a8302a64937ed87a4ec23fec03b2
Chen <renchenjun5370@gmail.com>
2023-02-17 18:22:22 -0500

    Adding initial entry point to the game with setting game to full screen

---
+++ app/src/main/java/me/game/cs2340/demo/MainActivity.java
@@ -0,0 +1,23 @@
+package me.game.cs2340.demo;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+/**
+ * MainActivity is the entry point to the game.
+ */
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+
+        //Sets flags to full screen mode
+        Window applicationWindow = this.getWindow();
+        applicationWindow.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+        );
+    }
+}
