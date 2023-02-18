ff0956d3b774c655daa34f39c0c615f7d70762dc
Chen <renchenjun5370@gmail.com>
2023-02-18 17:38:44 -0500

    Adding Game class that draws UPS and FPS to screen

--- app/src/main/java/me/game/cs2340/demo/Game.java
+++ app/src/main/java/me/game/cs2340/demo/Game.java
@@ -1,23 +1,35 @@
 package me.game.cs2340.demo;
 
 import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
 import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
 
 public class Game extends SurfaceView implements SurfaceHolder.Callback {
-    public Game(Context context){
+
+    private GameLoop gameLoop;
+    private Context context;
+
+    public Game(Context context) {
         super(context);
 
         //Gets the surface holder and adds callback to game
         SurfaceHolder surfaceHolder = this.getHolder();
         surfaceHolder.addCallback(this);
+
+        this.context = context;
+        this.gameLoop = new GameLoop(this, surfaceHolder);
+
+        this.setFocusable(true);
     }
 
     @Override
     public void surfaceCreated(SurfaceHolder surfaceHolder) {
-
+        this.gameLoop.startLoop();
     }
 
     @Override
@@ -27,6 +39,33 @@ public class Game extends SurfaceView implements SurfaceHolder.Callback {
 
     @Override
     public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
+
+    }
+
+    @Override
+    public void draw(Canvas canvas) {
+        super.draw(canvas);
+        this.drawUPS(canvas);
+        this.drawFPS(canvas);
+    }
+
+    public void drawUPS(Canvas canvas) {
+        String updatesPerSecond = Double.toString(gameLoop.getAverageUPS());
+        Paint paint = new Paint();
+        paint.setColor(ContextCompat.getColor(context, R.color.white));
+        paint.setTextSize(50);
+        canvas.drawText("UPS: " + updatesPerSecond, 100, 20, paint);
+    }
+
+    public void drawFPS(Canvas canvas) {
+        String framesPerSecond = Double.toString(gameLoop.getAverageFPS());
+        Paint paint = new Paint();
+        paint.setColor(ContextCompat.getColor(context, R.color.white));
+        paint.setTextSize(50);
+        canvas.drawText("FPS: " + framesPerSecond, 100, 40, paint);
+    }
+
+    public void update() {
 
     }
 }
