41607ed2505dfdca832749726ad46b684139cdb5
Chen <renchenjun5370@gmail.com>
2023-02-19 21:45:05 -0500

    Adding Joystick functionality to player class

--- app/src/main/java/me/game/cs2340/demo/Player.java
+++ app/src/main/java/me/game/cs2340/demo/Player.java
@@ -7,10 +7,14 @@ import android.graphics.Paint;
 import androidx.core.content.ContextCompat;
 
 public class Player {
+    private static final double SPEED_PIXELS_PER_SECOND = 400.0;
+    private static final double MAX_SPEED = SPEED_PIXELS_PER_SECOND / GameLoop.getMaxUPS();
     private double xPosition;
     private double yPosition;
     private double radius;
     private Paint paint;
+    private double xVelocity;
+    private double yVelocity;
 
     public Player(Context context, double xPosition, double yPosition, double radius) {
         this.setPosition(xPosition, yPosition);
@@ -18,14 +22,19 @@ public class Player {
 
         this.paint = new Paint();
         this.paint.setColor(ContextCompat.getColor(context, R.color.player));
+
     }
 
     public void draw(Canvas canvas) {
         canvas.drawCircle((float) xPosition, (float) yPosition, (float) radius, paint);
     }
 
-    public void update() {
+    public void update(Joystick joystick) {
+        xVelocity = joystick.getActuatorX() * MAX_SPEED;
+        yVelocity = joystick.getActuatorY() * MAX_SPEED;
 
+        xPosition += xVelocity;
+        yPosition += yVelocity;
     }
 
     public void setPosition(double xPosition, double yPosition) {
