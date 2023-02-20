c84b0893fb10697a1c1fe66680cc412d1afcdd7f
Chen <renchenjun5370@gmail.com>
2023-02-20 15:56:06 -0500

    Updating game entity to use euclidean distance calculation

--- app/src/main/java/me/game/cs2340/demo/GameEntity.java
+++ app/src/main/java/me/game/cs2340/demo/GameEntity.java
@@ -7,6 +7,8 @@ public abstract class GameEntity {
     protected double yPosition;
     protected double xVelocity;
     protected double yVelocity;
+    protected double xDirection;
+    protected double yDirection;
 
     public GameEntity(double xPosition, double yPosition) {
         this.xPosition = xPosition;
@@ -20,13 +22,18 @@ public abstract class GameEntity {
         return yPosition;
     }
 
+    public double getXDirection() {
+        return xDirection;
+    }
+
+    public double getYDirection() {
+        return yDirection;
+    }
+
     public abstract void draw(Canvas canvas);
     public abstract void update();
 
     public static double getEntityDistance(GameEntity obj1, GameEntity obj2) {
-        return Math.sqrt(
-                Math.pow(obj2.getXPosition() - obj1.getXPosition(), 2) +
-                        Math.pow(obj2.getYPosition() - obj1.getYPosition(), 2)
-        );
+        return Utility.getEuclideanDistance(obj1.getXPosition(), obj1.getYPosition(), obj2.getXPosition(), obj2.getYPosition());
     }
 }
--- app/src/main/java/me/game/cs2340/demo/Utility.java
+++ app/src/main/java/me/game/cs2340/demo/Utility.java
@@ -2,6 +2,6 @@ package me.game.cs2340.demo;
 
 public class Utility {
     public static double getEuclideanDistance(double point1_X, double point1_Y, double point2_X, double point2_Y) {
-        return Math.sqrt(Math.pow(point1_X - point2_X, 2) + Math.pow(point1_Y - point2_Y, 2))
+        return Math.sqrt(Math.pow(point1_X - point2_X, 2) + Math.pow(point1_Y - point2_Y, 2));
     }
 }
