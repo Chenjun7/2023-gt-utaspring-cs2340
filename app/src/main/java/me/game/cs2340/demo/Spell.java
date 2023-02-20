7b5e38e23275508d1daf82550ae8db3642b5a4e9
Chen <renchenjun5370@gmail.com>
2023-02-20 15:46:29 -0500

    Adding basic attack spell

---
+++ app/src/main/java/me/game/cs2340/demo/Spell.java
@@ -0,0 +1,21 @@
+package me.game.cs2340.demo;
+
+import android.content.Context;
+
+import androidx.core.content.ContextCompat;
+
+public class Spell extends CircleEntity {
+    public static final double SPEED_PIXELS_PER_SECOND = 800.0;
+    private static final double MAX_SPEED = SPEED_PIXELS_PER_SECOND / GameLoop.getMaxUPS();
+
+    public Spell(Context context, Player player) {
+        super(context, ContextCompat.getColor(context, R.color.spell), player.getXPosition(), player.getYPosition(), 25));
+        xVelocity = player.getXDirection() * MAX_SPEED;
+        yVelocity = player.getYDirection() * MAX_SPEED;
+    }
+    @Override
+    public void update() {
+        xPosition += xVelocity;
+        yPosition += yVelocity;
+    }
+}
