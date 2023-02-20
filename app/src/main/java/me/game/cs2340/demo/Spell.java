5fc8508eda062a15e37315011abb72592cb976de
Chen <renchenjun5370@gmail.com>
2023-02-20 15:57:01 -0500

    Updating Spell class to be more readable

--- app/src/main/java/me/game/cs2340/demo/Spell.java
+++ app/src/main/java/me/game/cs2340/demo/Spell.java
@@ -9,7 +9,8 @@ public class Spell extends CircleEntity {
     private static final double MAX_SPEED = SPEED_PIXELS_PER_SECOND / GameLoop.getMaxUPS();
 
     public Spell(Context context, Player player) {
-        super(context, ContextCompat.getColor(context, R.color.spell), player.getXPosition(), player.getYPosition(), 25));
+        super(context, ContextCompat.getColor(context, R.color.spell),
+                player.getXPosition(), player.getYPosition(), 25);
         xVelocity = player.getXDirection() * MAX_SPEED;
         yVelocity = player.getYDirection() * MAX_SPEED;
     }
